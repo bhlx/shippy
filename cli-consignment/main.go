c0e17e046c31d057564a6337e4765245e8291c9f
Benjamin Heller <hellerbenjamin@gmx.de>
2020-02-14 11:21:15 +0100

    update cli to new consignment service

--- cli-consignment/main.go
+++ cli-consignment/main.go
@@ -9,7 +9,8 @@ import (
 	"context"
 
 	pb "github.com/bhlx/shippy/consignment-service/proto/consignment"
-	"google.golang.org/grpc"
+
+	micro "github.com/micro/go-micro"
 )
 
 const (
@@ -28,13 +29,11 @@ func parseFile(file string) (*pb.Consignment, error) {
 }
 
 func main() {
-	// Set up a connection to the server.
-	conn, err := grpc.Dial(address, grpc.WithInsecure())
-	if err != nil {
-		log.Fatalf("Did not connect: %v", err)
-	}
-	defer conn.Close()
-	client := pb.NewShippingServiceClient(conn)
+
+	service := micro.NewService(micro.Name("shippy.consignment.cli"))
+	service.Init()
+
+	client := pb.NewShippingServiceClient("go.micro.srv.consignment", service.Client())
 
 	// Contact the server and print out its response.
 	file := defaultFilename
